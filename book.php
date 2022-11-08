7eed5c46c78dc9cfc29bfa9b752068e7b8867c9a
Jean-Baptiste Lavisse <jb@levillageducode.fr>
2022-11-08 12:04:43 +0100

    Add describe method in book.php

--- book.php
+++ book.php
@@ -10,12 +10,16 @@ class Book {
         $this->price = $price;
         $this->author = $author;
     }
+
+    function describe() {
+        echo "Le livre ".$this->title." écrit par ".$this->author." coûte ".$this->price."€".PHP_EOL;
+    }
 }
 
 $book1 = new Book("Clean Code","Robert C Martin",28.67);
 $book2 = new Book("JavaScript: the definitive guide", "David Flanagan",51.04);
 $book3 = new Book("Les chevaliers d'émeraude 1", "Anne Robillard", 15.30);
 
-var_dump($book1);
-var_dump($book2);
-var_dump($book3);
+$book1->describe();
+$book2->describe();
+$book3->describe();
